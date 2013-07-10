b755688ed746564d3cb27918bed3daa119ceba8d
Ignacio Huerta <ignacio@ihuerta.net>
2013-07-10 17:48:11 +0200

    Primer commit: crear la aplicación He ejecutado lo siguiente:    hobo new gestion_cursos --setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GestionCursos::Application.config.secret_token = '8aef120dd04e87055e8f80bba6a12ecc48b72dd1d1cbb5f4ebe02ad89eddf9da9fe7cf894a2e442e7db0ea74d4e4c913db9aaf51726a02d322f858041d496d8e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+GestionCursos::Application.config.session_store :cookie_store, :key => '_gestion_cursos_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# GestionCursos::Application.config.session_store :active_record_store
