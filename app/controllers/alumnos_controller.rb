9765d1137d4665377709f42ba6d39d62f2ead983
Ignacio Huerta <ignacio@ihuerta.net>
2013-07-11 10:20:32 +0200

    Reparar el test El test fallaba porque después de crear un alumno, el sistema nos llevaba a la página del alumno. Si desactivamos la página del alumno conseguimos que vuelva directamente a la página del curso. Y ya que hemos desactivado la página del alumno, mejoramos un poco la card para poder editar el nombre, teléfono e email directamente

--- app/controllers/alumnos_controller.rb
+++ app/controllers/alumnos_controller.rb
@@ -5,5 +5,11 @@ class AlumnosController < ApplicationController
   auto_actions :all, :except => [:index]
   
   auto_actions_for :curso, :create
+  
+  def create_for_curso
+    hobo_create do
+      redirect_to @alumno.curso
+    end
+  end
 
 end
