1301534b4e01057cac200957ab648e77a7be40f7
Ignacio Huerta <ignacio@ihuerta.net>
2013-07-11 07:47:12 +0200

    Traducir la aplicación a castellano.
    En primer lugar hay que descargar estos dos ficheros y guardarlos dentro de config/locales:
      https://raw.github.com/svenfuchs/rails-i18n/master/rails/locale/es.yml
      https://raw.github.com/Hobo/hobo/master/hobo/lib/generators/hobo/i18n/templates/hobo.es.yml
    
    Luego hay que indicar en config/application.rb que queremos que el idioma por defecto sea el castellano. Reiniciar la aplicación y listo!
    
    Cuando queremos saber cómo traducir un mensaje, podemos activar también "config.hobo.show_translation_keys" y meter esas traducciones dentro de config/locales/app.es.yml

--- config/hobo_routes.rb
+++ config/hobo_routes.rb
@@ -5,6 +5,19 @@
 GestionCursos::Application.routes.draw do
 
 
+  # Resource routes for controller alumnos
+  resources :alumnos, :only => [:new, :edit, :show, :create, :update, :destroy]
+
+  # Owner routes for controller alumnos
+  resources :cursos, :as => :curso, :only => [] do
+    resources :alumnos, :only => [] do
+      collection do
+        post 'create', :action => 'create_for_curso'
+      end
+    end
+  end
+
+
   # Resource routes for controller users
   resources :users, :only => [:edit, :show, :create, :update, :destroy] do
     collection do
@@ -24,24 +37,11 @@ GestionCursos::Application.routes.draw do
   match 'forgot_password(.:format)' => 'users#forgot_password', :as => 'user_forgot_password'
 
 
-  # Resource routes for controller alumnos
-  resources :alumnos, :only => [:new, :edit, :show, :create, :update, :destroy]
-
-  # Owner routes for controller alumnos
-  resources :cursos, :as => :curso, :only => [] do
-    resources :alumnos, :only => [] do
-      collection do
-        post 'create', :action => 'create_for_curso'
-      end
-    end
-  end
+  # Resource routes for controller categories
+  resources :categories
 
 
   # Resource routes for controller cursos
   resources :cursos
-
-
-  # Resource routes for controller categories
-  resources :categories
 
 end
